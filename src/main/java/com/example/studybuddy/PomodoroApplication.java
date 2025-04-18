dad5466929558934cbecf467d7e1d7ef4613f99d
veysel73 <158464410+veysel73@users.noreply.github.com>
2025-04-18 16:17:18 +0300

    Dosyaları doğrudan projeye aldım

---
+++ src/main/java/com/example/studybuddy/PomodoroApplication.java
@@ -0,0 +1,45 @@
+package com.example.studybuddy;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class PomodoroApplication extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/studybuddy/PomodoroView.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/com/example/studybuddy/Pomodoro.css").toExternalForm());
+
+            // Make window draggable
+            root.setOnMousePressed(event -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            });
+            root.setOnMouseDragged(event -> {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            });
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Pomodoro Timer");
+            primaryStage.initStyle(StageStyle.UNDECORATED); // Removes default window decoration
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/example/studybuddy/loginApplication.java
@@ -0,0 +1,20 @@
+package com.example.studybuddy;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class loginApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Login Ekranı");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
